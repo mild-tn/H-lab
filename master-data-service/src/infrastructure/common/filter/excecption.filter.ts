c15966a55b4e3124ad63d16196580f0ef0bbf6dc
Tassamild <tassaneewan.no@easysunday.com>
2024-12-02 21:16:06 +0700

    Add AllExceptionFilter for handling exceptions and logging

---
+++ master-data-service/src/infrastructure/common/filter/excecption.filter.ts
@@ -0,0 +1,45 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { LoggerService } from 'src/infrastructure/services/logger/logger.service';
+
+@Catch()
+export class AllExceptionFilter implements ExceptionFilter {
+  constructor(private readonly loggerService: LoggerService) {}
+
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message =
+      exception instanceof HttpException
+        ? exception.getResponse()
+        : 'Internal server error';
+
+    // Log the exception
+    this.loggerService.error(
+      AllExceptionFilter.name,
+      JSON.stringify({
+        message,
+        status,
+      }),
+    );
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message,
+    });
+  }
+}
